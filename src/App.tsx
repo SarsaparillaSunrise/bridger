9854cd8198d695fc03361c6921dae436c5819b18
SarsaparillaSunrise <149747120+SarsaparillaSunrise@users.noreply.github.com>
2024-05-14 10:54:27 +1200

    Implemented search functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -41,10 +41,7 @@ const Home = ({clickHandler}) => {
   )
 }
 
-const Item = (category, item) => {
-  const [modalStatus, setModalStatus] = useState(false)
-  const toggleModal = () => setModalStatus(!modalStatus)
-
+const Item = (category, item, modalStatus, toggleModal) => {
   return (
     <div key={item.name}>
       <button className="item-button" onClick={toggleModal}>{item.name}</button>
@@ -58,14 +55,36 @@ const Item = (category, item) => {
               <IntakeForm item={item} toggleModal={toggleModal} />
           }
           </div>
-          : ''
+          : null
       }
     </div>
   )
 }
 
 const Search = ({category, items}) => {
-  const search = (e) => console.log(e.key)
+  const [results, setResults] = useState(items);
+  const [query, setQuery] = useState('')
+  const [modalStatus, setModalStatus] = useState(false)
+  const toggleModal = () => setModalStatus(!modalStatus)
+
+
+
+  const search = (e) => {
+    if (/^[a-zA-Z]$/.test(e.key)) {
+      const q = query + e.key;
+      const matches = results.filter((match) => match.name.startsWith(q));
+      setQuery(q);
+      setResults(matches);
+    }
+    else if (e.key == 'Backspace') {
+      const q = query.substring(0, query.length - 1)
+      const matches = items.filter((result) => result.name.startsWith(q));
+      setQuery(q)
+      setResults(matches);
+    }
+
+  }
+
   return (
       <>
         <form className="search-form">
@@ -73,7 +92,7 @@ const Search = ({category, items}) => {
         </form>
         <div className="search-results">
         <ul>
-          {items.map((item) => Item(category, item))}
+          {results.map((result) => Item(category, result, modalStatus, toggleModal))}
         </ul>
       </div>
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Prevent browser from stealing focus upon encountering writev/write
-  // server: {hmr: false},
+  server: {
+    // Prevent browser from stealing focus upon encountering writev/write
+    // hmr: false
+    host: "0.0.0.0",
+    port: 3000,
+  },
+
 })
