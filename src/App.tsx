f2d00a3747121892a5492d6659d62589cfa47ddf
SarsaparillaSunrise <149747120+SarsaparillaSunrise@users.noreply.github.com>
2024-05-13 14:45:16 +1200

    Added dummy data.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,36 @@
 import { useState } from 'react'
 import './App.css'
 
+const lifts = [
+  {
+    "id": 1,
+    "name": "squat",
+  },
+  {
+    "id": 2,
+    "name": "deadlift",
+  },
+  {
+    "id": 3,
+    "name": "bench press",
+  }
+]
+
+const foods = [
+  {
+    "id": 1,
+    "name": "coffee",
+  },
+  {
+    "id": 2,
+    "name": "pistachio nuts",
+  },
+  {
+    "id": 3,
+    "name": "toast",
+  }
+]
+
 const Home = ({clickHandler}) => {
   return (
     <>
@@ -10,10 +40,19 @@ const Home = ({clickHandler}) => {
   )
 }
 
-const Search = () => (<h1>Search</h1>)
+const Search = ({category, items}) => {
+  return (
+  <>
+    <h1>Search</h1>
+    <h2>{category}</h2>
+    <h2>{JSON.stringify(items)}</h2>
+  </>
+  )
+}
 
 const App = () => {
   const [category, setCategory] = useState(null)
+  const data = {'exercise': lifts, 'intake': foods}
 
   return (
     <div className="container">
@@ -21,7 +60,7 @@ const App = () => {
       category == null ?
         <Home clickHandler={setCategory}/>
       :
-        <Search />
+        <Search category={category} items={data[category]} />
       }
 
     </div>
