f943c51303fec9650a06253480d94d086db72918
SarsaparillaSunrise <149747120+SarsaparillaSunrise@users.noreply.github.com>
2024-05-19 16:39:18 +1200

    Ran prettier.

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,59 @@
-import { useState, useEffect } from 'react'
-import { ExerciseForm, BeverageForm, FoodForm, getItems} from './Forms';
-import './App.css'
+import { useState, useEffect } from "react";
+import { ExerciseForm, BeverageForm, FoodForm, getItems } from "./Forms";
+import "./App.css";
 
-
-const Home = ({clickHandler}) => {
+const Home = ({ clickHandler }) => {
   return (
     <div className="categories">
-      <button className="category-selection" onClick={() => clickHandler('exercise')}>Exercise</button>
-      <button className="category-selection" onClick={() => clickHandler('consumable')}>Consumable</button>
+      <button
+        className="category-selection"
+        onClick={() => clickHandler("exercise")}
+      >
+        Exercise
+      </button>
+      <button
+        className="category-selection"
+        onClick={() => clickHandler("consumable")}
+      >
+        Consumable
+      </button>
     </div>
-  )
-}
+  );
+};
 
 const formRenderer = (item, toggleModal) => {
-    if (item.category == 'Food') {
-      return <FoodForm item={item} toggleModal={toggleModal} />
-    } else if (item.category == 'Beverage') {
-      return <BeverageForm item={item} toggleModal={toggleModal} />
-    } else {
-    return <ExerciseForm item={item} toggleModal={toggleModal} />
+  if (item.category == "Food") {
+    return <FoodForm item={item} toggleModal={toggleModal} />;
+  } else if (item.category == "Beverage") {
+    return <BeverageForm item={item} toggleModal={toggleModal} />;
+  } else {
+    return <ExerciseForm item={item} toggleModal={toggleModal} />;
   }
-}
+};
 
 const Item = (item, modalStatus, toggleModal) => {
   return (
     <div key={item.name}>
-      <button className="item-button" onClick={() => toggleModal(item.name)}>{item.name}</button>
-      {
-        modalStatus == item.name ?
-          <div className="overlay">
-          {
-            formRenderer(item, toggleModal)
-          }
-          </div>
-          : null
-      }
+      <button className="item-button" onClick={() => toggleModal(item.name)}>
+        {item.name}
+      </button>
+      {modalStatus == item.name ? (
+        <div className="overlay">{formRenderer(item, toggleModal)}</div>
+      ) : null}
     </div>
-  )
-}
+  );
+};
 
-const Search = ({items}) => {
+const Search = ({ items }) => {
   const [results, setResults] = useState(items);
-  const [modalStatus, setModalStatus] = useState(null)
+  const [modalStatus, setModalStatus] = useState(null);
 
-  const search = (e) => setResults(items.filter(
-    (match) => match.name.toLowerCase() .startsWith(e.target.value.toLowerCase())));
+  const search = (e) =>
+    setResults(
+      items.filter((match) =>
+        match.name.toLowerCase().startsWith(e.target.value.toLowerCase()),
+      ),
+    );
 
   return (
     <>
@@ -57,34 +66,35 @@ const Search = ({items}) => {
         </ul>
       </div>
     </>
-  )
-}
+  );
+};
 
 const App = () => {
-  const [category, setCategory] = useState(null)
-  const [data, setData] = useState()
+  const [category, setCategory] = useState(null);
+  const [data, setData] = useState();
 
   // TODO: Check this doesn't run twice in prod
   useEffect(() => {
-    const fetchData = async () => 
-      setData({'exercise': await getItems('exercise'), 'consumable': await getItems('consumable')});
+    const fetchData = async () =>
+      setData({
+        exercise: await getItems("exercise"),
+        consumable: await getItems("consumable"),
+      });
 
-    fetchData()
-        .catch(console.error);;
-    }, [])
+    fetchData().catch(console.error);
+  }, []);
 
   return (
-      <div className="container">
-        <main className="main">
-          {
-          category == null ?
-            <Home clickHandler={setCategory}/>
-          :
-            <Search items={data[category]} />
-          }
-        </main>
-      </div>
-  )
-}
+    <div className="container">
+      <main className="main">
+        {category == null ? (
+          <Home clickHandler={setCategory} />
+        ) : (
+          <Search items={data[category]} />
+        )}
+      </main>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/Forms.tsx
+++ src/Forms.tsx
@@ -1,23 +1,37 @@
-const ExerciseForm = ({item, toggleModal}) => {
+const ExerciseForm = ({ item, toggleModal }) => {
   const postForm = async (formData) => {
-    await postFormData('workout', {
-      "exercise_id": formData.get('exercise_id'),
-      "volume": formData.get('volume'),
-      "reps": formData.get('reps'),
-      "notes": formData.get('notes'),
-    })
-    toggleModal()
-  }
+    await postFormData("workout", {
+      exercise_id: formData.get("exercise_id"),
+      volume: formData.get("volume"),
+      reps: formData.get("reps"),
+      notes: formData.get("notes"),
+    });
+    toggleModal();
+  };
   return (
     <>
       <h1 className="form-title">{item.name}</h1>
-      <span className="close-modal" onClick={toggleModal}><p>&times;</p></span>
+      <span className="close-modal" onClick={toggleModal}>
+        <p>&times;</p>
+      </span>
       <form className="entry-form" action={postForm}>
-        <input name="exercise_id" type="hidden" defaultValue={item.id} readOnly />
+        <input
+          name="exercise_id"
+          type="hidden"
+          defaultValue={item.id}
+          readOnly
+        />
         <p>
           <label htmlFor="volume">Volume:</label>
         </p>
-        <input name="volume" type="number" min="1" inputMode="numeric" required autoFocus />
+        <input
+          name="volume"
+          type="number"
+          min="1"
+          inputMode="numeric"
+          required
+          autoFocus
+        />
         <p>
           <label htmlFor="reps">Rep count:</label>
         </p>
@@ -26,80 +40,113 @@ const ExerciseForm = ({item, toggleModal}) => {
           <label htmlFor="notes">Notes:</label>
         </p>
         <input name="notes" type="textArea" />
-        <button className="submit-form" type="submit">Submit</button>
+        <button className="submit-form" type="submit">
+          Submit
+        </button>
       </form>
     </>
   );
-}
+};
 
-const FoodForm = ({item, toggleModal}) => {
+const FoodForm = ({ item, toggleModal }) => {
   const postForm = async (formData) => {
-    await postFormData('intake', {
-      "consumable_id": formData.get('consumable_id'),
-      "volume": formData.get('volume'),
-    })
-    toggleModal()
-  }
+    await postFormData("intake", {
+      consumable_id: formData.get("consumable_id"),
+      volume: formData.get("volume"),
+    });
+    toggleModal();
+  };
   return (
     <>
       <h1 className="form-title">{item.name}</h1>
-      <span className="close-modal" onClick={toggleModal}><p>&times;</p></span>
+      <span className="close-modal" onClick={toggleModal}>
+        <p>&times;</p>
+      </span>
       <form className="entry-form" action={postForm}>
-        <input name="consumable_id" type="hidden" defaultValue={item.id} readOnly />
+        <input
+          name="consumable_id"
+          type="hidden"
+          defaultValue={item.id}
+          readOnly
+        />
         <p>
           <label htmlFor="volume">Volume (g):</label>
         </p>
-        <input name="volume" type="number" min="1" inputMode="numeric" autoFocus />
-        <button className="submit-form" type="submit">Submit</button>
+        <input
+          name="volume"
+          type="number"
+          min="1"
+          inputMode="numeric"
+          autoFocus
+        />
+        <button className="submit-form" type="submit">
+          Submit
+        </button>
       </form>
     </>
   );
-}
+};
 
-const BeverageForm = ({item, toggleModal}) => {
+const BeverageForm = ({ item, toggleModal }) => {
   const postForm = async (formData) => {
-    await postFormData('intake', {
-      "consumable_id": formData.get('consumable_id'),
-      "volume": formData.get('volume'),
-    })
-    toggleModal()
-  }
+    await postFormData("intake", {
+      consumable_id: formData.get("consumable_id"),
+      volume: formData.get("volume"),
+    });
+    toggleModal();
+  };
   return (
     <>
       <h1 className="form-title">{item.name}</h1>
-      <span className="close-modal" onClick={toggleModal}><p>&times;</p></span>
+      <span className="close-modal" onClick={toggleModal}>
+        <p>&times;</p>
+      </span>
       <form className="entry-form" action={postForm}>
-        <input name="consumable_id" type="hidden" defaultValue={item.id} readOnly />
+        <input
+          name="consumable_id"
+          type="hidden"
+          defaultValue={item.id}
+          readOnly
+        />
         <p>
           <label htmlFor="volume">Volume (ml):</label>
         </p>
-        <input name="volume" type="number" min="1" inputMode="numeric" autoFocus />
-        <button className="submit-form" type="submit">Submit</button>
+        <input
+          name="volume"
+          type="number"
+          min="1"
+          inputMode="numeric"
+          autoFocus
+        />
+        <button className="submit-form" type="submit">
+          Submit
+        </button>
       </form>
     </>
   );
-}
+};
 
-const baseUri = 'http://127.0.0.1:8000/'
+const baseUri = "http://127.0.0.1:8000/";
 const postOptions = {
-  method: 'POST',
-  mode: 'cors',
+  method: "POST",
+  mode: "cors",
   headers: {
-      "Content-Type": "application/json",
-    },
-  body: null
-}
+    "Content-Type": "application/json",
+  },
+  body: null,
+};
 
-const getItems = async (category) => fetch(baseUri + category, {mode: 'cors'})
-  .then(response => response.json())
-  .catch(error => console.error(error));
+const getItems = async (category) =>
+  fetch(baseUri + category, { mode: "cors" })
+    .then((response) => response.json())
+    .catch((error) => console.error(error));
 
 const postFormData = async (category, formData) => {
-  postOptions["body"] = JSON.stringify(formData)
-  return fetch(baseUri + category, postOptions).then(response => response.json())
-    .then(data => console.log(data.data))
-    .catch(error => console.error(error));
-}
+  postOptions["body"] = JSON.stringify(formData);
+  return fetch(baseUri + category, postOptions)
+    .then((response) => response.json())
+    .then((data) => console.log(data.data))
+    .catch((error) => console.error(error));
+};
 
-
-export {ExerciseForm, FoodForm, BeverageForm, getItems}
+export { ExerciseForm, FoodForm, BeverageForm, getItems };
