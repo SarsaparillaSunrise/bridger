0f8cdffb32a47b455f791ed42951038594b82108
SarsaparillaSunrise <149747120+SarsaparillaSunrise@users.noreply.github.com>
2024-06-08 21:43:55 +1200

    Moved workout creation to service layer.

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,13 +1,12 @@
 from os import environ
 from typing import List
 
-from domain import Workout
 from fastapi import Depends, FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from models import Consumable as ConsumableModel
 from models import Intake
 from repository import SQLAlchemyRepository
-from services import list_consumables, list_exercises
+from services import add_workout, list_consumables, list_exercises
 from sqlalchemy import create_engine
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session, declarative_base, sessionmaker
@@ -93,5 +92,4 @@ async def intake_create(intake: IntakeCreate, session: Session = Depends(get_db)
 
 @app.post("/workout", response_model=WorkoutRead, status_code=201)
 async def workout_create(workout: WorkoutCreate, session: Session = Depends(get_db)):
-    repository = SQLAlchemyRepository(session)
-    return repository.add(Workout(**workout.model_dump()))
+    return add_workout(session=session, workout=workout)
--- src/backend/services.py
+++ src/backend/services.py
@@ -1,5 +1,10 @@
-from domain import Consumable, Exercise
+from domain import Consumable, Exercise, Workout
 from repository import SQLAlchemyRepository
+
+
+def add_workout(session, workout):
+    repository = SQLAlchemyRepository(session)
+    return repository.add(Workout(**workout.model_dump()))
 
 
 def list_consumables(session):
