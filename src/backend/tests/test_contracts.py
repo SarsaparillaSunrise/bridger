9f367988e93c3ed7f1e4918c28e2f76019c2bda8
SarsaparillaSunrise <149747120+SarsaparillaSunrise@users.noreply.github.com>
2024-06-09 14:22:26 +1200

    Tidied Act and Assert sections of Contract tests.

--- src/backend/tests/test_contracts.py
+++ src/backend/tests/test_contracts.py
@@ -1,59 +1,31 @@
 def test_read_item_consumable(test_client) -> None:
     response = test_client.get(url="consumable")
+    assert response.json() == [dict(id=1, category="FOOD", name="Test Food")]
     assert response.status_code == 200
-    assert response.json() == [
-        {
-            "id": 1,
-            "category": "FOOD",
-            "name": "Test Food",
-        }
-    ]
 
 
 def test_read_item_exercise(test_client) -> None:
     response = test_client.get(url="exercise")
-    assert response.status_code == 200
     assert response.json() == [
-        {
-            "id": 1,
-            "category": "Compound Lift",
-            "name": "Test Exercise",
-        }
+        dict(id=1, category="Compound Lift", name="Test Exercise")
     ]
+    assert response.status_code == 200
 
 
 def test_create_workout_entry(test_client) -> None:
-    response = test_client.post(
-        url="workout",
-        json={
-            "exercise_id": 1,
-            "volume": "120",
-            "reps": "5",
-            "notes": "test",
-        },
-    )
+    expected = dict(exercise_id=1, volume=120, reps=5, notes="test")
+    response = test_client.post(url="workout", json=expected)
+    assert response.json() == expected
     assert response.status_code == 201
-    assert response.json() == {
-        "exercise_id": 1,
-        "volume": 120,
-        "reps": 5,
-        "notes": "test",
-    }
 
 
 def test_create_intake_entry(test_client, session) -> None:
-    response = test_client.post(
-        url="intake",
-        json={"consumable_id": "1", "volume": "120"},
-    )
-    assert response.status_code == 201
+    response = test_client.post(url="intake", json=dict(consumable_id=1, volume=120))
     assert response.json() == {"id": 1, "volume": 120}
+    assert response.status_code == 201
 
 
 def test_create_intake_entry_fails_with_invalid_consumable(test_client) -> None:
-    response = test_client.post(
-        url="intake",
-        json={"consumable_id": "1000", "volume": "120", "reps": "5", "notes": "test"},
-    )
-    assert response.status_code == 422
+    response = test_client.post(url="intake", json=dict(consumable_id=1000, volume=100))
     assert response.json() == {"detail": "Integrity Error"}
+    assert response.status_code == 422
