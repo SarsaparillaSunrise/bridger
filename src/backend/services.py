ce7a254a057e17a886232e8e9a630d0d006c6b44
SarsaparillaSunrise <149747120+SarsaparillaSunrise@users.noreply.github.com>
2024-06-12 19:21:13 +1200

    Moved validators into service layer.

--- src/backend/services.py
+++ src/backend/services.py
@@ -3,30 +3,46 @@ from sqlalchemy.orm import Session
 
 from domain import Consumable, Exercise, Intake, Workout
 from repository import SQLAlchemyRepository
+from validators import (
+    ConsumableRead,
+    ExerciseRead,
+    IntakeCreate,
+    IntakeRead,
+    WorkoutRead,
+)
 
 
-def add_intake(session: Session, intake: Intake) -> Intake:
+def add_intake(session: Session, intake: IntakeCreate) -> IntakeRead:
     repository: SQLAlchemyRepository = SQLAlchemyRepository(session)
     consumable: Consumable = repository.get(
         model=Consumable, record_id=intake.consumable_id
     )
     if not consumable:
         raise HTTPException(status_code=422, detail="Integrity Error")
+    record = repository.create(Intake(consumable=consumable, volume=intake.volume))
+    record.calculate_intake_presentation_values()
+    return IntakeRead(consumable=consumable, id=record.id, volume=record.volume)
 
-    intake = Intake(consumable=consumable, volume=intake.volume)
-    return repository.create(intake)
 
-
-def add_workout(session, workout):
+def add_workout(session, workout) -> WorkoutRead:
     repository = SQLAlchemyRepository(session)
-    return repository.create(Workout(**workout.model_dump()))
+    record = repository.create(Workout(**workout.model_dump()))
+    return WorkoutRead(
+        id=record.id,
+        exercise_id=record.exercise_id,
+        volume=record.volume,
+        reps=record.reps,
+        notes=record.notes,
+    )
 
 
-def list_consumables(session):
+def list_consumables(session) -> ConsumableRead:
     repository = SQLAlchemyRepository(session)
-    return repository.list(Consumable)
+    records = repository.list(Consumable)
+    return [ConsumableRead(**c.__dict__) for c in records]
 
 
 def list_exercises(session):
     repository = SQLAlchemyRepository(session)
-    return repository.list(Exercise)
+    records = repository.list(Exercise)
+    return [ExerciseRead(**e.__dict__) for e in records]
