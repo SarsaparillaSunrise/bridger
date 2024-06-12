0da38bd00a15d09536924a7e54d1eb52ee2efd35
SarsaparillaSunrise <149747120+SarsaparillaSunrise@users.noreply.github.com>
2024-06-12 19:17:37 +1200

    Implemented business logic for food/beverage storage units.

--- src/backend/domain.py
+++ src/backend/domain.py
@@ -74,16 +74,34 @@ class Intake:
     """
     Table to store all consumption
 
+    Intake isn't a value object because some fields are computed.
+
+    Food is given in Grams and is stored in Milligrams
+    Beverages are given in Millilitres and stored in Millilitres
+
+    This is currently reconciled for presentation in `calculate_intake_presentation_values`
+
     Attributes:
         consumable: a Consumable entry
-        volume: amount consumed
+        volume: amount consumed in milligrams if the Consumable category is FOOD; in millitres otherwise
     """
 
     def __init__(self, consumable: Consumable, volume: int) -> None:
         self._consumable = consumable
+        self._volume = volume
         self.consumable_id = consumable.id
-        self.volume = volume * 1000  # Convert to MG/ML
+        self.volume = self._calculate_total_volume()
         self.calories = self._calculate_total_calories()
 
     def _calculate_total_calories(self):
         return int(self._consumable.calories * self.volume / 100_000.0)
+
+    def _calculate_total_volume(self):
+        if self._consumable.category == CategoryConsumable.FOOD:
+            return self._volume * 1000
+        return self._volume
+
+    def calculate_intake_presentation_values(self):
+        if self._consumable.category == CategoryConsumable.FOOD:
+            self.volume /= 1000
+        return self
