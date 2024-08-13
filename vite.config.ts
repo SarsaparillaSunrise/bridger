ed02b0f167ff1849fd4fcae991137042e7306dfe
SarsaparillaSunrise <149747120+SarsaparillaSunrise@users.noreply.github.com>
2024-08-13 15:25:45 +1200

    Installed Tailwind.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "tailwindcss";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +10,11 @@ export default defineConfig({
     setup: "./setupTest.ts",
   },
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
   server: {
     // Prevent browser from stealing focus upon encountering writev/write
     // hmr: false
