63e089573b68aa9ab1760edee815ce56d1921722
SarsaparillaSunrise <149747120+SarsaparillaSunrise@users.noreply.github.com>
2024-08-20 11:05:13 +1200

    Added Consumable search results test.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,16 +7,13 @@ import { Search } from "./App";
 // console.log(screen.debug());
 
 describe("Search", () => {
-  beforeEach(() => {
+  test("backend response exercises are present in search results", async () => {
     global.fetch = vi.fn().mockResolvedValue({
       json: vi.fn().mockResolvedValue([
         { id: 1, name: "Test Excercise 1" },
         { id: 2, name: "Test Excercise 2" },
       ]),
     });
-  });
-
-  test("backend response exercises are present in search results", async () => {
     render(
       <MemoryRouter
         initialEntries={[{ pathname: "/search", state: "exercise" }]}
@@ -32,6 +29,31 @@ describe("Search", () => {
       );
       expect(screen.getByText("Test Excercise 1"));
       expect(screen.getByText("Test Excercise 2"));
+    });
+  });
+
+  test("backend response consumables are present in search results", async () => {
+    global.fetch = vi.fn().mockResolvedValue({
+      json: vi.fn().mockResolvedValue([
+        { id: 1, name: "Test Consumable 1" },
+        { id: 2, name: "Test Consumable 2" },
+      ]),
+    });
+    render(
+      <MemoryRouter
+        initialEntries={[{ pathname: "/search", state: "consumable" }]}
+      >
+        <Search />
+      </MemoryRouter>,
+    );
+
+    await waitFor(() => {
+      expect(fetch).toHaveBeenCalledWith(
+        `${import.meta.env.VITE_UPSTREAM_ROOT}consumable`,
+        {},
+      );
+      expect(screen.getByText("Test Consumable 1"));
+      expect(screen.getByText("Test Consumable 2"));
     });
   });
 });
