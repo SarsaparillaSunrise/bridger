114ef544f5bdb294591ca678dfd5b91b1bc39525
SarsaparillaSunrise <149747120+SarsaparillaSunrise@users.noreply.github.com>
2024-10-01 17:54:39 +1300

    Added migration for inserted_at column on workouts table.

---
+++ src/backend/alembic/versions/3c61c6cefac6_add_inserted_at_to_workout.py
@@ -0,0 +1,35 @@
+"""Add inserted_at to workout
+
+Revision ID: 3c61c6cefac6
+Revises: bf85b1d6bd24
+Create Date: 2024-09-28 16:16:14.592329
+
+"""
+
+from typing import Sequence, Union
+
+import sqlalchemy as sa
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = "3c61c6cefac6"
+down_revision: Union[str, None] = "bf85b1d6bd24"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column(
+        "workouts",
+        sa.Column(
+            "inserted_at",
+            sa.DateTime(timezone=True),
+            nullable=False,
+            server_default=sa.func.timezone("UTC", sa.func.current_timestamp()),
+        ),
+    )
+
+
+def downgrade() -> None:
+    op.drop_column("workouts", "inserted_at")
