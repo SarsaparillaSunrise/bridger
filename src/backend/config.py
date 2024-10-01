a756aadb59e44f714aee5224eedd39332f3cc0bc
SarsaparillaSunrise <149747120+SarsaparillaSunrise@users.noreply.github.com>
2024-10-01 17:56:05 +1300

    Updated config to point to Postgres DB.

--- src/backend/config.py
+++ src/backend/config.py
@@ -1,1 +1,6 @@
-DATABASE_URL = "./db.sqlite3"
+import os
+
+DATABASE_URL = os.environ.get(
+    "DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/bridger"
+)
+TEST_DATABASE_URL = DATABASE_URL + "_test"
