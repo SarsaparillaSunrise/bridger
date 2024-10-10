357d9ff41151431dc3e2e99dba42df6a03cff805
SarsaparillaSunrise <149747120+SarsaparillaSunrise@users.noreply.github.com>
2024-10-10 15:01:36 +1300

    Updated domain tests to reflect new fixtures.

--- src/backend/tests/test_domain.py
+++ src/backend/tests/test_domain.py
@@ -1,21 +1,21 @@
 from domain.model import Intake
 
 
-def test_food_intake_volume_stored_in_milligrams(food_fixture):
-    sut = Intake(consumable=food_fixture, volume=300)
+def test_food_intake_volume_stored_in_milligrams(foods):
+    sut = Intake(consumable=foods[0], volume=300)
     assert sut.volume == 300_000
 
 
-def test_food_intake_volume_presentation_is_in_grams(food_fixture):
-    sut = Intake(consumable=food_fixture, volume=300)
+def test_food_intake_volume_presentation_is_in_grams(foods):
+    sut = Intake(consumable=foods[0], volume=300)
     assert sut.calculate_intake_presentation_values().volume == 300
 
 
-def test_beverage_intake_volume_stored_in_millilitres(beverage_fixture):
-    sut = Intake(consumable=beverage_fixture, volume=300)
+def test_beverage_intake_volume_stored_in_millilitres(beverages):
+    sut = Intake(consumable=beverages[0], volume=300)
     assert sut.volume == 300
 
 
-def test_beverage_intake_volume_presentation_is_in_millilitres(beverage_fixture):
-    sut = Intake(consumable=beverage_fixture, volume=300)
+def test_beverage_intake_volume_presentation_is_in_millilitres(beverages):
+    sut = Intake(consumable=beverages[0], volume=300)
     assert sut.calculate_intake_presentation_values().volume == 300
